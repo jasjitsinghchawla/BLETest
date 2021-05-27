60e160774d89fad7f855130d5777658fe36a0b6b
jasjitsinghchawla <63247443+jasjitsinghchawla@users.noreply.github.com>
2021-05-27 09:04:16 +0530

    Initial Commit

---
+++ BLETest/BLETestApp.swift
@@ -0,0 +1,20 @@
+//
+//  BLETestApp.swift
+//  BLETest
+//
+//  Created by Jasjit Singh Chawla on 27/05/21.
+//
+
+import SwiftUI
+
+@main
+struct BLETestApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
